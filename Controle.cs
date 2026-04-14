05b3c4ca13378248fa6db27a57c8ef883601b10d
Marcos Firmino <marquinhosrodrig1022@gmail.com>
2026-04-13 21:48:35 -0300

    Add files via upload

---
+++ Controle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unar
+{
+    class Controle
+    {
+        public static int abacate = 0;
+        public static int banana = 0;
+        public static int laranja = 0;
+        public static int goiaba = 0;
+        public static int total = 0; 
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timer1.Interval == 3000)
+            {
+                votacao frmvota = new votacao();
+                frmvota.Show();
+                timer1.Enabled = false;
+            }
+        }
+    }
+}
